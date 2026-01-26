988a4d8d1873975ac537679f5b9c062ad801f9dd
GustavoHRX <gustavohrx123@gmail.com>
2026-01-25 21:49:30 -0300

    TestingFirstCommit

---
+++ src/main/java/com/ghr/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+
+
+package com.ghr.helloworld;
+
+/**
+ *
+ * @author gusta
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Ola, me chamo Gustavo");
+    }
+}
